9fc99e329690bedc481c795b1fd4a500e692e395
Stackbit Code Editor <support@stackbit.com>
2021-03-08 20:28:16 +0000

    TypingEffect.js: changed

--- src/components/TypingEffect.js
+++ src/components/TypingEffect.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
 
+import PropTypes from "prop-types";
+
 export default class TypingEffect extends React.Component {
     render() {
         let action = _.get(this.props, 'action', null);
