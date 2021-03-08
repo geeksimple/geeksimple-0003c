fd5ad30b3ab979416c2d7e7bf3065fed7980bb4d
Stackbit Code Editor <support@stackbit.com>
2021-03-08 21:10:43 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -41,7 +41,7 @@ export default class HeroSection extends React.Component {
                     </div>
                     )}
                     <div className="section__actions btn-group">
-                      
+                      <TypingEffect />
                     </div>
                   </div>
                 </div>
