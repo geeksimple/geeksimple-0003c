c7572f744519398b3d0aaf0a2498802f2f1cfbf7
Stackbit Code Editor <support@stackbit.com>
2021-03-08 21:48:15 +0000

    TypingEffect.js: changed

--- src/components/TypingEffect.js
+++ src/components/TypingEffect.js
@@ -12,7 +12,7 @@ export default class TypingEffect extends React.Component {
 
   static defaultProps = {
     text:
-      "Dies",
+      "Die beste Grundlagen für jedes Buzzword Bingo",
     wordsPerSecond: 20
   };
 
