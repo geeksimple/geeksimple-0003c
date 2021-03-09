2777d67b7dba5058c09b2136d7647a11064fc472
Stackbit Code Editor <support@stackbit.com>
2021-03-09 21:44:15 +0000

    TypingEffect.js: changed

--- src/components/TypingEffect.js
+++ src/components/TypingEffect.js
@@ -12,7 +12,7 @@ export default class TypingEffect extends React.Component {
 
   static defaultProps = {
     text:
-      "Hier werden die Mysterien rund um IT-Begriffe geklärt.",
+      "",
     wordsPerSecond: 20
   };
 
