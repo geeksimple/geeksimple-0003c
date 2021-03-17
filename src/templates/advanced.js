0b2d27fa79e14e7541f31bdee2b59525ec32493e
Stackbit Code Editor <projects@stackbit.com>
2021-03-17 20:18:46 +0000

    advanced.js: changed

--- src/templates/advanced.js
+++ src/templates/advanced.js
@@ -36,7 +36,7 @@ export default class Advanced extends React.Component {
                     buttonClasses="btn"
                     containerClasses=""
                     contentClasses="text-capitalize"
-                    style={{ background: "#98756F" }}
+                    style={{ background: "#98756F", fontSize: ""}}
                     buttonStyle={{ background: "#F19E8E", color: "#FFF", fontSize: "20px" }}
                     expires={999}
                     overlay={true}
