e5c6fcc083e26ac9e32e75eeec8c094bc6089622
Stackbit Code Editor <projects@stackbit.com>
2021-03-21 14:57:35 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -39,7 +39,6 @@ export default class Body extends React.Component {
                     {_.get(this.props, 'pageContext.site.siteMetadata.favicon', null) && (
                     <link rel="icon" href={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.favicon', null))}/>
                     )}
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
