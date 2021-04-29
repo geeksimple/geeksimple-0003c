207bd7a28286b295a27530137825482d3d056b5c
Stackbit Code Editor <projects@stackbit.com>
2021-04-29 06:54:41 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -17,7 +17,7 @@ export default class ContactSection extends React.Component {
                   </div>
                   <div className="form-group">
                     <label id="email-label" htmlFor="email">Email</label>
-                    <input aria-labelledby="email-label" type="email" name="email" id="email" placeholder="Your email" required />
+                    <input aria-labelledby="email-label" type="email" name="email" id="email" placeholder="Deine " required />
                   </div>
                   <div className="form-group">
                     <label id="message-label" htmlFor="message">Nachricht</label>
